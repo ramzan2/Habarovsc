2b0e1d56c6de7dfa5cd9f919dce39ad1efd8d202
mollaev <mollaev@tk34.mskobr.ru>
2022-04-25 12:20:22 +0300

    Добавьте файлы проекта.

---
+++ Habarovsc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Habarovsc
+{
+    class Program
+    {
+        public static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите конечное значение диапазона 1...N и нажмите Enter");
+            Console.WriteLine("N = ");
+            if ((!int.TryParse(Console.ReadLine(), out int result)) || (result < 0))
+                Console.WriteLine("Число должно быть положительным и целым");
+            Console.WriteLine($"Простые числа из диапазона от 1 до {result}");
+            int count = 0;
+            for (int i = 1; i <= result; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write($"{i} ");
+                    count++;
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Найдено {count} простых чисел из диапазона от 1 до {result}");
+        }
+    }
+}
